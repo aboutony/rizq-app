ce7429d2477b4151f1afc42878204ceb3431266e
aboutony <138366071+aboutony@users.noreply.github.com>
2026-02-04 01:09:39 +0300

    Fix import path for globals.css in layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import "../globals.css";
+import "../../globals.css";
 
 export default async function LocaleLayout({
   children,

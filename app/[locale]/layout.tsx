94450501aa1a058027ac428c23042d2840945076
aboutony <138366071+aboutony@users.noreply.github.com>
2026-02-04 01:54:09 +0300

    Replace globals.css with theme.css in layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import "../../globals.css";
+import "../../theme.css";
 
 export default async function LocaleLayout({
   children,

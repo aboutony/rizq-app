bb1ea8ff5abceb43344a87f2c0c140666e9d0dfb
aboutony <138366071+aboutony@users.noreply.github.com>
2026-02-09 18:28:40 +0300

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,4 +3,3 @@ import { redirect } from 'next/navigation';
 export default function LocaleIndex({ params }: { params: { locale: string } }) {
   redirect(`/${params.locale}/login`);
 }
-export { default } from './dashboard/page';
